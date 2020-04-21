8e4d6ab5f4bb2314341e9473e22dcc29c3704afe
DopplerKuo <doppler.kuo@gmail.com>
2020-04-21 16:56:59 +0800

    refactor all.js

--- all.js
+++ all.js
@@ -1,100 +1,63 @@
-$(document).ready(function(){
-
-    
-    
-
-    
-    // 分類表單2
-
-    $(function() {
-        $.get('https://spreadsheets.google.com/feeds/list/1df73uUaBPzRKgLm8-Vcnsc59dW6kzudAQKFRhjN_CGI/2/public/values?alt=json', function(data) {
+$(document).ready(function () {
+  // 分類表單2
+  $(function () {
+    $.get(
+      'https://spreadsheets.google.com/feeds/list/1df73uUaBPzRKgLm8-Vcnsc59dW6kzudAQKFRhjN_CGI/2/public/values?alt=json',
+      function (data) {
         console.log(data);
-         
-        var d = data.feed.entry;
-        var items = [];
-        for(var i in d) {
-        var item = {};
-        item.catename = d[i].gsx$catename.$t;
-        item.catemin = d[i].gsx$catemin.$t;
-        items.push(item);
-        }
-        
-          
-           
-        for(var i in items) {
-                    var Card = `
-                    <label for="${items[i].catemin}" class="cate1"><input type="checkbox" id="${items[i].catemin}" checked><span class="arrow"></span><p class="catename">${items[i].catemin}</p>
-                    <div class="toggle-box">
-                    <div class="${items[i].catemin}"></div>
-                    </div>
-                    </label>
-                          
-                        
-                  ` ;
-                    $('.test').append(Card);
-                  }
-        
-       
-        
-    
-    
-    
-    
-    
-    
-               
+
+        var feedData = data.feed.entry;
+        var items = feedData.map(function (item) {
+          return {
+            catename: item.gsx$catename.$t,
+            catemin: item.gsx$catemin.$t,
+          };
         });
+
+        items.forEach(function (item) {
+          var Card = `
+            <label for="${item.catemin}" class="cate1">
+            <input type="checkbox" id="${item.catemin}" checked>
+            <span class="arrow"></span>
+            <p class="catename">${item.catemin}</p>
+            <div class="toggle-box">
+            <div class="${item.catemin}"></div>
+            </div>
+            </label>
+          `;
+          $('.test').append(Card);
         });
-    
+      }
+    );
+  });
 
-        // 給資料表單1
-        $(function() {
-            $.get('https://spreadsheets.google.com/feeds/list/1df73uUaBPzRKgLm8-Vcnsc59dW6kzudAQKFRhjN_CGI/1/public/values?alt=json', function(data) {
-            console.log(data);
-             
-            var d = data.feed.entry;
-            var items = [];
-            for(var i in d) {
-            var item = {};
-            item.name = d[i].gsx$name.$t;
-            item.video = d[i].gsx$video.$t;
-            item.image = d[i].gsx$image.$t;
-            item.content = d[i].gsx$content.$t;
-            item.catemin = d[i].gsx$catemin.$t;
-            items.push(item);
-            }
-            
-            console.table(items);
-              
-               
-            for(var i in items) {
-                        var Card = `
-                        <li><a href="#">${items[i].name}</a></li>
-                              
-                            
-                      ` ;
-                        $(`.${items[i].catemin}`).append(Card);
-                      }
-            
-           
-            
-        
-        
-        
-        
-        
-        
-                   
-            });
-            });
+  // 給資料表單1
+  $(function () {
+    $.get(
+      'https://spreadsheets.google.com/feeds/list/1df73uUaBPzRKgLm8-Vcnsc59dW6kzudAQKFRhjN_CGI/1/public/values?alt=json',
+      function (data) {
+        console.log(data);
 
+        var feedData = data.feed.entry;
+        var items = feedData.map(function (item) {
+          return {
+            name: item.gsx$name.$t,
+            video: item.gsx$video.$t,
+            image: item.gsx$image.$t,
+            content: item.gsx$content.$t,
+            catemin: item.gsx$catemin.$t,
+          };
+        });
 
-
-
-
-    
-    
-    
-    
-});
-
+        items.forEach(function (item) {
+          var Card = `
+            <li>
+              <a href="#">${item.name}</a>
+            </li>    
+          `;
+          $(`.${item.catemin}`).append(Card);
+        });
+      }
+    );
+  });
+})
